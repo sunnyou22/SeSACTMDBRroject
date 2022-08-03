a796a9e26aed5481b0a57821acfd16ffd207a259
sunnyou22 <sunnyouyaya22@gmail.com>
2022-08-04 02:59:12 +0900

    feat: TMDB 미니프로젝트 UI구성

---
+++ SeSACTMDBRroject/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  SeSACTMDBRroject
+//
+//  Created by 방선우 on 2022/08/04.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ SeSACTMDBRroject/Resource/Protocol.swift
@@ -0,0 +1,19 @@
+//
+//  Protocol.swift
+//  SeSACTMDBRroject
+//
+//  Created by 방선우 on 2022/08/04.
+//
+
+import Foundation
+import UIKit
+
+protocol ReusableIdentifier {
+    static var reuseIdentifer: String { get }
+}
+
+extension UIViewController: ReusableIdentifier {
+    static var reuseIdentifer: String {
+        return String(describing: self)
+    }
+}
