85b5bd7ac4e89c02bcf792e6f54bba0b2f6f8031
sunnyou22 <sunnyouyaya22@gmail.com>
2022-08-03 09:24:27 +0900

    Initial Commit

---
+++ SeSACTMDBRroject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACTMDBRroject
+//
+//  Created by 방선우 on 2022/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
